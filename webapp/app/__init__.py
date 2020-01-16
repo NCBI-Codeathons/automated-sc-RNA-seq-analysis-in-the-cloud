7d2e1bd56922c593e8723ceca0c6819cfd390651
aopisco <angela.oliveira.pisco@gmail.com>
2020-01-16 17:09:50 -0500

    changed context folders

---
+++ webapp/app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+#change upload folder
+UPLOAD_FOLDER = '../'
+app = Flask(__name__)
+
+from app import routes2
+#from app import routes
+
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = "secret key"
+#app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
---
+++ webapp/cellxgene_launcher.py
@@ -0,0 +1,14 @@
+import subprocess
+import os
+# e.g., subprocess.call(['df', '-h'])
+
+cwd = os.getcwd()
+
+subprocess.run(['docker', 'run', \
+    '--mount', f'type=bind,source={cwd}/data/adata_small_test.h5ad,target=/input', \
+    '--mount', f'type=bind,source={cwd}/data/output/output_processed.h5ad,target=/output.h5ad', \
+    'czbiohub/sc-rna-seq-processing:0.0.2'],
+    check = True)
+
+
+subprocess.run(['cellxgene','launch', 'data/output/output_processed.h5ad', '--open'])
