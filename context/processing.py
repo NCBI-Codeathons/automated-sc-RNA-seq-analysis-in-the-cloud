a6400053c417e3da4222b137c8c8cfe16901cf8d
mccalluc <mccalluc@users.noreply.github.com>
2020-01-16 10:47:17 -0500

    last fixes by hand

--- context/processing.py
+++ context/processing.py
@@ -1,10 +1,10 @@
 import argparse
 import scanpy as sc
-import numpy
+# import numpy
 import IPython.display as IP
-import igraph
-import louvain
-import leidenalg
+# import igraph
+# import louvain
+# import leidenalg
 
 
 def main(
@@ -30,9 +30,10 @@ def main(
 
     print('normalization & scaling')
     sc.pp.normalize_per_cell(adata, counts_per_cell_after=1e4)
-    adata_nonans = sc.pp.filter_genes_dispersion(adata, subset=False, min_disp=.5, max_disp=None,
-                                                 min_mean=.0125, max_mean=10, n_bins=20, n_top_genes=None,
-                                                 log=True, copy=True)
+    adata_nonans = sc.pp.filter_genes_dispersion(
+        adata, subset=False, min_disp=.5, max_disp=None,
+        min_mean=.0125, max_mean=10, n_bins=20, n_top_genes=None,
+        log=True, copy=True)
     sc.pp.log1p(adata)
     sc.pp.scale(adata, max_value=10, zero_center=False)
 
