495ebf7a222f37fce3d0bab8c219f3ac25838e0f
Sanjana Shah <42432922+shahsanjana@users.noreply.github.com>
2020-01-16 16:13:40 -0500

    Shahsanjana/webapp (#14)
    * updated webapp
    
    * deleted routes.py
    
    * updated init

---
+++ webapp/app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+#change upload folder
+UPLOAD_FOLDER = '/tmp'
+app = Flask(__name__)
+
+from app import routes2
+#from app import routes
+
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = "secret key"
+#app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+
+
