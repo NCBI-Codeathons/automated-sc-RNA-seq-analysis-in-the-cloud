74c1f80ad6204e7b83fde355918dd57d7a050f80
Chuck McCallum <mccalluc@users.noreply.github.com>
2020-07-01 18:28:14 -0400

    local tests (#18)
    * test -> test-ci
    
    * chmod
    
    * background flask

---
+++ test-local.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+
+./test-ci.sh
+./download-data.sh
+./build-and-run-image.sh
+# TODO: Make assertion about output.
+
+cd webapp
+./start.sh &
+# TODO: Wait for Flask to start and check response.
